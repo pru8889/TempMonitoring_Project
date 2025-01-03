df9990e1f4d2a18f966dc58f6a7b0c441db4d17d
Pruthvi Raj Gujjeti <pruthvi.raj.gujjeti@gmail.com>
2025-01-03 21:26:55 +0530

    Clean up header files

--- sw/include/tempMonitor.h
+++ sw/include/tempMonitor.h
@@ -1,22 +1,18 @@
 #ifndef TEMPMONITOR_H
 #define TEMPMONITOR_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Include necessary standard libraries
+// Include standard libraries
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <adcsampling.h>
 
-// Define any constants
+// Define constants
 #define MAX_TEMP_SENSORS 10
 #define OPTIMAL_TEMP 85
 #define WARNING_TEMP 105
 
-// Declare any function prototypes
+// Declare function prototypes
 void temperature_RangeCheck(float temperature);
 
 
